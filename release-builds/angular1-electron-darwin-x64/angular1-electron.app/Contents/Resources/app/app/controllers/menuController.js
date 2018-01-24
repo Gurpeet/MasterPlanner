f584933e6c6034014924f3d6fdf83cb05d3bb787
gopisivia@gmail.com <gopisivia@gmail.com>
2018-01-24 23:22:53 +0530

    removed package setup

--- release-builds/angular1-electron-darwin-x64/angular1-electron.app/Contents/Resources/app/app/controllers/menuController.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    angular.module('app')
-        .controller('menuController', ['$stateParams', function ($stateParams) {
-            let self = this;
-            self.id = $stateParams.id;
-        }])
-})();
